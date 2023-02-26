5fc420665f8e15cd2735d966f4b3b1c3ee520a68
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-25 22:01:06 -0300

    commit 1

---
+++ teste/CentroDeCusto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    public class CentroDeCusto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public decimal VerbaLiberada { get; set; }
+        public int AnoVerbaLiberada { get; set; }
+
+        public CentroDeCusto
+            (int Id,
+            string descricao, 
+            DateTime dataCriacao, 
+            decimal verbaLiberada, 
+            int anoVerbaLiberada) 
+        {
+            Id = Id;
+            Descricao = descricao;  
+            DataCriacao = dataCriacao;
+            VerbaLiberada= verbaLiberada;  
+            AnoVerbaLiberada= anoVerbaLiberada;
+        }
+
+
+    }
+}
--- teste/Program.cs
+++ teste/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using teste;
 Console.WriteLine("Hello, World!");
+CentroDeCusto exemplo1 = new CentroDeCusto(1,"Teste", DateTime.Now, 1500.0M, 2023);
